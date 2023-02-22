e48ba12e98e580cda54d14a4c1b5ba1d2969d6c7
Odd <oddare@live.no>
2023-02-22 19:06:00 +0100

    Trying a different way

--- ProgrammeringPriv/IBE152Eksamener/2023-02-20/oppg5.py
+++ ProgrammeringPriv/IBE152Eksamener/2023-02-20/oppg5.py
@@ -1,13 +1,22 @@
 def boxmaker(n: int, a: int, b: int):
     box = []
     box += ['*']*n + ['\n']
-    for i in range(n):
-        box += ['*']+[' ']*(n-2) + ['*'] + ['\n']
+    for i in range(n-2):
+        box += ['*']+[' ']*(n) + ['*'] + ['\n']
     box += ['*']*n
     print(box)
+    aUse = (a + 1) + (a // (n - 2))
+    bUse = b + 1
+    coordinate = aUse * bUse * (n + 1)
+    box[coordinate] = 'x'
+
     boxShow = ''
     for i in box:
         boxShow += i
     print(boxShow)
 
-boxmaker(8, 1, 4)
+#boxmaker(8, 1, 1)
+
+
+def try2(n: int, a: int, b: int):
+    
