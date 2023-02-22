08e3e2783b6d84bb09e655fe0fd44079b779f139
Odd <oddare@live.no>
2023-02-22 19:11:47 +0100

    Closer

--- ProgrammeringPriv/IBE152Eksamener/2023-02-20/oppg5.py
+++ ProgrammeringPriv/IBE152Eksamener/2023-02-20/oppg5.py
@@ -19,4 +19,10 @@ def boxmaker(n: int, a: int, b: int):
 
 
 def try2(n: int, a: int, b: int):
-    
+    for i in range(n):
+        for j in range(n):
+            if i == 0 or j == 0 or i == n-1:
+                print('*', end='')
+        print()
+
+try2(4, 1, 1)
