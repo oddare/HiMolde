37f299dce0a10a770ae7dd9fb12d3139cfacb952
Odd <oddare@live.no>
2023-02-22 19:17:14 +0100

    Now puts a star on desired location, we want X

--- ProgrammeringPriv/IBE152Eksamener/2023-02-20/oppg5.py
+++ ProgrammeringPriv/IBE152Eksamener/2023-02-20/oppg5.py
@@ -21,7 +21,7 @@ def boxmaker(n: int, a: int, b: int):
 def try2(n: int, a: int, b: int):
     for i in range(n):
         for j in range(n):
-            if i == 0 or j == 0 or i == n-1 or j == n-1:
+            if i == 0 or j == 0 or i == n-1 or j == n-1 or (i == a and j == b):
                 print('*', end='')
             else:
                 print(' ', end='')
