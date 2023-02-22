1effa51e069cf41cd0b3010f3e27cc72da8dfe98
Odd <oddare@live.no>
2023-02-22 18:50:04 +0100

    Well, it makes a box

--- ProgrammeringPriv/IBE152Eksamener/2023-02-20/oppg5.py
+++ ProgrammeringPriv/IBE152Eksamener/2023-02-20/oppg5.py
@@ -1,9 +1,13 @@
 def boxmaker(n: int, a: int, b: int):
     box = []
+    box += ['*']*n + ['\n']
+    for i in range(n):
+        box += ['*']+[' ']*(n-2) + ['*'] + ['\n']
     box += ['*']*n
     print(box)
+    boxShow = ''
     for i in box:
-        boxShow = i
+        boxShow += i
     print(boxShow)
 
 boxmaker(8, 1, 4)
