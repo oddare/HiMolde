4b6b9ee8088c938016358f39e31fd7e4a3357297
Odd <oddare@live.no>
2023-02-22 16:09:52 +0100

    Moving along

--- ProgrammeringPriv/IBE152Eksamener/2023-02-20/oppg4.py
+++ ProgrammeringPriv/IBE152Eksamener/2023-02-20/oppg4.py
@@ -1,9 +1,16 @@
-def timeToDestination(busSchedule: list, time: int, timeLimit: int):
-    schedule = []
-    return schedule
+def timeToDestination(busSchedule: list, time: int, timeLimit: int) -> list:
+    timeschedule = []
+    for n in range(len(busSchedule)):
+        if busSchedule[n][1] >= time:
+            timeschedule.append(busSchedule[n])
+    limitschedule = []
+    for n in range(len(timeschedule)):
+        if timeschedule[n][2] <= timeLimit:
+            limitschedule.append(timeschedule[n])
+    return limitschedule
 
 buses = [("701a", 10, 20), ("702a", 20, 15), ("703a", 0, 10), ("704", 38, 13)]
-now = 05
+now = 5
 limit = 50
 
 print(timeToDestination(buses, now, limit))
