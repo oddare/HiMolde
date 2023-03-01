0d903ea4406fd4d2865ddd1a02935248710923a2
Odd <oddare@live.no>
2023-03-01 23:43:44 +0000

    Mar 2, 2023, 12:43 AM

--- ProgrammeringPriv/IBE152Eksamener/Tutorial Exam 1/excercise5.py
+++ ProgrammeringPriv/IBE152Eksamener/Tutorial Exam 1/excercise5.py
@@ -1,6 +1,6 @@
 def weekDay(day: str, num: int) -> int:
     list = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     val =  list.index(day)
-    return val
+    return 
 
 print(weekDay('Tue', 1))
