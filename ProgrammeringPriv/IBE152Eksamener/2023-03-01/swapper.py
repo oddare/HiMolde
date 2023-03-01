4bedcf8e11771637858cdd8de4d80b888884e25c
Odd <oddare@live.no>
2023-03-01 21:31:23 +0100

    Returning answer

--- ProgrammeringPriv/IBE152Eksamener/2023-03-01/swapper.py
+++ ProgrammeringPriv/IBE152Eksamener/2023-03-01/swapper.py
@@ -1,7 +1,7 @@
 def swapper(s: str, c: str, pos: int) -> str:
     first = s[0:pos]
     end = s[pos-1: -1]
-    print(end)
+    return f'{first}{c}{end}'
 
 
 print(swapper('....', 'A', 2)) # -> ..A..
