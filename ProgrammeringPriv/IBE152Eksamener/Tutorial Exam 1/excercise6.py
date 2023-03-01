92e9d1a8fc2bc9fe6e2389ea42b90aad7758f055
Odd <oddare@live.no>
2023-03-01 23:46:00 +0000

    Mar 2, 2023, 12:46 AM

---
+++ ProgrammeringPriv/IBE152Eksamener/Tutorial Exam 1/excercise6.py
@@ -0,0 +1,8 @@
+#This was swapper
+def swapper(s: str, c: str, pos: int) -> str:
+    first = s[0:pos]
+    end = s[pos-1: -1]
+    return f'{first}{c}{end}'
+
+
+print(swapper('....', 'A', 2)) # -> ..A..
