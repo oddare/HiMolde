6cbce5405ab40d85788a1e475fce6b4981b95360
Odd <oddare@live.no>
2023-03-02 00:19:54 +0000

    Mar 2, 2023, 1:19 AM

--- ProgrammeringPriv/IBE152Eksamener/Samlehefte/O2.py
+++ ProgrammeringPriv/IBE152Eksamener/Samlehefte/O2.py
@@ -2,7 +2,7 @@ def clock(seconds: int) -> str:
     hours = int((seconds // 3600))
     minutes = int((seconds // 60) % 60)
     seconds = int(seconds % 60)
-    return f''
+    return f'{hours}:{minutes}:{seconds}'
 
 
 print(clock(3600*2.87))
