4896f9ac81882924a78dd36e161fa4eda73ab32f
Odd <oddare@live.no>
2023-03-02 01:12:14 +0000

    Mar 2, 2023, 2:12 AM

---
+++ ProgrammeringPriv/IBE152Eksamener/Samlehefte/O6.py
@@ -0,0 +1,16 @@
+def triangle(a: int, b: int, c: int) -> str:
+    if not a + b > c or b + c > a or c + a > b:
+        return 'Invalid'
+    if a == b == c:
+        return 'Equilateral'
+    if a == b and b != c or b == c and b != a or a == c and a != b:
+        return 'Isosceles'
+    if a != b and b != c and c != a:
+        return 'Scalene'
+    return
+
+
+print(triangle(2, 7, 12))
+print(triangle(3, 3, 3))
+print(triangle(12, 7, 12))
+print(triangle(10, 5, 9))
