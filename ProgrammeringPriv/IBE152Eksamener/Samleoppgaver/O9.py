f1a307161e286323fc3aa5ff0c814145b234d5e7
Odd <oddare@live.no>
2023-03-02 15:12:40 +0000

    Mar 2, 2023, 4:12 PM

--- ProgrammeringPriv/IBE152Eksamener/Samleoppgaver/O9.py
+++ ProgrammeringPriv/IBE152Eksamener/Samleoppgaver/O9.py
@@ -2,3 +2,7 @@ def program():
     list = [1, 2, 9362, 4, 5]
     studnum = 48234 #Not real, this is random
     index = list.index(9362)
+    list.insert(index+1, studnum)
+    list.pop
+
+    print(list)
