cc055ade888faf7caafbc727c3255037d9075ec4
Odd <oddare@live.no>
2023-03-02 01:24:13 +0000

    Mar 2, 2023, 2:24 AM

--- ProgrammeringPriv/IBE152Eksamener/Samlehefte/O7.py
+++ ProgrammeringPriv/IBE152Eksamener/Samlehefte/O7.py
@@ -1,6 +1,6 @@
 def conversion():
     miles = input('Enter the distance in miles: ')
-    meters = miles * 1609.44
+    meters = float(miles) * 1609.44
     print(f'The distance in meters is: {meters}')
 
 
