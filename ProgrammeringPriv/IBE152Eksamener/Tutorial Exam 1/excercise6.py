48e6ed917d6ff5242846ab0e0db5d2956b538093
Odd <oddare@live.no>
2023-03-02 00:04:38 +0000

    Mar 2, 2023, 1:04 AM

--- ProgrammeringPriv/IBE152Eksamener/Tutorial Exam 1/excercise6.py
+++ ProgrammeringPriv/IBE152Eksamener/Tutorial Exam 1/excercise6.py
@@ -1,3 +1,10 @@
+"""
+Write a function that receives a string s, a character c and a valid position 
+in the string pos and returns a new string equal to the one received except for the 
+element at position pos that is equal to c.
+
+replace(".....", "A", 2) -> "..A.."
+"""
 #This was swapper
 def swapper(s: str, c: str, pos: int) -> str:
     first = s[0:pos]
--- ProgrammeringPriv/IBE152Eksamener/Tutorial Exam 1/excercise7.py
+++ ProgrammeringPriv/IBE152Eksamener/Tutorial Exam 1/excercise7.py
@@ -1,3 +1,10 @@
+"""
+You're given a sorted list of numbers in a sequence from 1 to n with one of the numbers 
+missing (number 1 is always present). 
+Design a function that takes a sorted list as argument and returns the 
+number that is missing from the list. 
+(Application of Gauss method for the summation 1..n)
+"""
 def numCheck(sortedList: list) -> int:
     endNum = len(sortedList) + 1
     gauss = endNum / 2 * (endNum + 1)
