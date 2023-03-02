f20fc9c538b6e265312061ef7c7ac584b9ee2fae
Odd <oddare@live.no>
2023-03-02 17:21:35 +0000

    Mar 2, 2023, 6:21 PM

--- ProgrammeringPriv/IBE152Eksamener/Samleoppgaver/O12.py
+++ ProgrammeringPriv/IBE152Eksamener/Samleoppgaver/O12.py
@@ -1,4 +1,9 @@
 def budget_OK(budget: int, participants: int) -> bool:
+    cost = 100
+    if 5 < participants <= 10:
+        cost += 20
+    if participants > 10:
+        cost += 30
 
 
 print(budget_OK(0, 1))      #False
