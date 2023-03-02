cdf72a8b5a5d4893420b39815ea615213d341222
Odd <oddare@live.no>
2023-03-02 15:23:57 +0000

    Mar 2, 2023, 4:23 PM

--- ProgrammeringPriv/IBE152Eksamener/Samleoppgaver/O11.py
+++ ProgrammeringPriv/IBE152Eksamener/Samleoppgaver/O11.py
@@ -6,3 +6,4 @@ def secret(paragraph: str, pairs: list) -> str:
 
 
 print(secret('Batman will try', [[0, 0], [4, 4], [9, 10]])) #Ball
+print(secret('Snow or chocolate is the question', [[1, 3], [17, 20], [13, 16]]))
